065b24d54fe97e121a5fa0e5c11e0147657a0821
Chao An <jasonan0715@gmail.com>
2023-12-12 00:32:40 -0500

    Fixed all probs for us bar

--- js/barus.js
+++ js/barus.js
@@ -1,112 +1,112 @@
 var data3= [
-    {States:"Alabama",Population:5024279},
-    {States:"Alaska",Population:731158},
-	{States:"Arizona",Population:7378491},
-    {States:"Arkansas",Population:3024692},
-    {States:"California",Population:39512223},
-    {States:"Colorado",Population:5758736},
-    {States:"Connecticut",Population:3565287},
-    {States:"Delaware",Population:973764},
-    {States:"Florida",Population:21477737},
-    {States:"Georgia",Population:10617423},
-    {States:"Hawaii",Population:1415872}, 
-    {States:"Idaho",Population:1787065},
-    {States:"Illinois",Population:12671821},
-    {States:"Indiana",Population:6732219},
-    {States:"Iowa",Population:3155070},
-    {States:"Kansas",Population:2913314},
-    {States:"Kentucky",Population:4467673},
-	{States:"Louisiana",Population:4648794},
-	{States:"Maine",Population:1344212},
-	{States:"Maryland",Population:6045680},
-	{States:"Massachusetts",Population:6892503},
-	{States:"Michigan",Population:10045029},
-	{States:"Minnesota",Population:5639632},
-	{States:"Mississippi",Population:2976149},
-	{States:"Missouri",Population:6137428},
-	{States:"Montana",Population:1068778},
-	{States:"Nebraska",Population:1934408},
-	{States:"Nevada",Population:3155070},
-	{States:"New Hampshire",Population:1359711},
-	{States:"New Jersey",Population:8882190},
-	{States:"New Mexico",Population:2096829},
-	{States:"New York",Population:19453561},
-	{States:"North Carolina",Population:10488084},
-	{States:"North Dakota",Population:762062},
-	{States:"Ohio",Population:11689100},
-	{States:"Oklahoma",Population:3956971},
-	{States:"Oregon",Population:4217737},
-	{States:"Pennsylvania",Population:12801989},
-	{States:"Rhode Island",Population:1059361},
-	{States:"South Carolina",Population:5148714},
-	{States:"South Dakota",Population:884659},
-	{States:"Tennessee",Population:6829174},
-	{States:"Texas",Population:28995881},
-	{States:"Utah",Population:3205958},
-	{States:"Vermont",Population:8535519},
-	{States:"Virginia",Population:502651},
-	{States:"Washington",Population:7614893},
-	{States:"West Virginia",Population:1792147},
-	{States:"Wisconsin",Population:5822434},
-	{States:"Wyoming",Population:578759}
+    {USStates:"Alabama",Population:5024279, StateInitial:"AL", popHolder:5024279},
+    {USStates:"Alaska",Population:731158, StateInitial:"AK",popHolder:731158},
+	{USStates:"Arizona",Population:7378491, StateInitial:"AZ",popHolder:7378491},
+    {USStates:"Arkansas",Population:3024692, StateInitial:"AR"  ,popHolder:3024692},
+    {USStates:"California",Population:39512223, StateInitial:"CA" ,popHolder:39512223},
+    {USStates:"Colorado",Population:5758736, StateInitial:"CO"  ,popHolder:5758736},
+    {USStates:"Connecticut",Population:3565287, StateInitial:"CT" ,popHolder:3565287},
+    {USStates:"Delaware",Population:973764, StateInitial:"DE"   ,popHolder:973764},
+    {USStates:"Florida",Population:21477737,  StateInitial:"FL" ,popHolder:21477737},
+    {USStates:"Georgia",Population:10617423, StateInitial:"GA"  ,popHolder:10617423},
+    {USStates:"Hawaii",Population:1415872,  StateInitial:"HI"  ,popHolder:1415872},
+    {USStates:"Idaho",Population:1787065, StateInitial:"ID"   ,popHolder:1787065},
+    {USStates:"Illinois",Population:12671821, StateInitial:"IL" ,popHolder:12671821},
+    {USStates:"Indiana",Population:6732219, StateInitial:"IN"  ,popHolder:6732219},
+    {USStates:"Iowa",Population:3155070,  StateInitial:"IA"   ,popHolder:3155070},
+    {USStates:"Kansas",Population:2913314,  StateInitial:"KS" ,popHolder:2913314},
+    {USStates:"Kentucky",Population:4467673, StateInitial:"KY"  ,popHolder:4467673},
+	{USStates:"Louisiana",Population:4648794, StateInitial:"LA" ,popHolder:4648794},
+	{USStates:"Maine",Population:1344212, StateInitial:"ME"    ,popHolder:1344212},
+	{USStates:"Maryland",Population:6045680, StateInitial:"MD"  ,popHolder:6045680},
+	{USStates:"Massachusetts",Population:6892503, StateInitial:"MA" ,popHolder:6892503},
+	{USStates:"Michigan",Population:10045029, StateInitial:"MI" ,popHolder:10045029},
+	{USStates:"Minnesota",Population:5639632, StateInitial:"MN" ,popHolder:5639632},
+	{USStates:"Mississippi",Population:2976149, StateInitial:"MS" ,popHolder:2976149},
+	{USStates:"Missouri",Population:6137428, StateInitial:"MO"  ,popHolder:6137428},
+	{USStates:"Montana",Population:1068778, StateInitial:"MT"  ,popHolder:1068778},
+	{USStates:"Nebraska",Population:1934408, StateInitial:"NE"  ,popHolder:1934408},
+	{USStates:"Nevada",Population:3155070, StateInitial:"NV"    ,popHolder:3155070},
+	{USStates:"New Hampshire",Population:1359711, StateInitial:"NH" ,popHolder:1359711},
+	{USStates:"New Jersey",Population:8882190, StateInitial:"NJ"  ,popHolder:8882190},
+	{USStates:"New Mexico",Population:2096829, StateInitial:"NM"  ,popHolder:2096829},
+	{USStates:"New York",Population:19453561, StateInitial:"NY" ,popHolder:19453561},
+	{USStates:"North Carolina",Population:10488084, StateInitial:"NC" ,popHolder:10488084},
+	{USStates:"North Dakota",Population:762062, StateInitial:"ND" ,popHolder:762062},
+	{USStates:"Ohio",Population:11689100, StateInitial:"OH" ,popHolder:11689100},
+	{USStates:"Oklahoma",Population:3956971, StateInitial:"OK"  ,popHolder:3956971},
+	{USStates:"Oregon",Population:4217737, StateInitial:"OR"  ,popHolder:4217737},
+	{USStates:"Pennsylvania",Population:12801989, StateInitial:"PA" ,popHolder:12801989},
+	{USStates:"Rhode Island",Population:1059361, StateInitial:"RI"    ,popHolder:1059361},
+	{USStates:"South Carolina",Population:5148714, StateInitial:"SC"  ,popHolder:5148714},
+	{USStates:"South Dakota",Population:884659, StateInitial:"SD" ,popHolder:884659},
+	{USStates:"Tennessee",Population:6829174, StateInitial:"TN" ,popHolder:6829174},
+	{USStates:"Texas",Population:28995881, StateInitial:"TX"  ,popHolder:28995881},
+	{USStates:"Utah",Population:3205958, StateInitial:"UT"  ,popHolder:3205958},
+	{USStates:"Vermont",Population:8535519, StateInitial:"VT" ,popHolder:8535519},
+	{USStates:"Virginia",Population:502651, StateInitial:"VA" ,popHolder:502651},
+	{USStates:"Washington",Population:7614893, StateInitial:"WA"  ,popHolder:7614893},
+	{USStates:"West Virginia",Population:1792147, StateInitial:"WV" ,popHolder:1792147},
+	{USStates:"Wisconsin",Population:5822434, StateInitial:"WI" ,popHolder:5822434},
+	{USStates:"Wyoming",Population:578759, StateInitial:"WY"  ,popHolder:578759}
     ];
 /* https://www.ahd.com/state_statistics.html */
 
 var data4= [
-    {States:"Alabama",Population:5024279,PatientInHospital:268332},
-    {States:"Alaska",Population:731158,PatientInHospital:2840210},
-	{States:"Arizona",Population:7378491,PatientInHospital:2947182},
-    {States:"Arkansas",Population:3024692,PatientInHospital:1460895},
-    {States:"California",Population:39512223,PatientInHospital:15165283},
-    {States:"Colorado",Population:5758736,PatientInHospital:972907},
-    {States:"Connecticut",Population:3565287,PatientInHospital:1815725},
-    {States:"Delaware",Population:973764,PatientInHospital:567555},
-    {States:"Florida",Population:21477737,PatientInHospital:12807644},
-    {States:"Georgia",Population:10617423,PatientInHospital:5158604},
-    {States:"Hawaii",Population:1415872,PatientInHospital:540167}, 
-    {States:"Idaho",Population:1787065,PatientInHospital:533269},
-    {States:"Illinois",Population:12671821,PatientInHospital:5808174},
-    {States:"Indiana",Population:6732219,PatientInHospital:3274050},
-    {States:"Iowa",Population:3155070,PatientInHospital:1230728},
-    {States:"Kansas",Population:2913314,PatientInHospital:1207621},
-    {States:"Kentucky",Population:4467673,PatientInHospital:2659947},
-	{States:"Louisiana",Population:4648794,PatientInHospital:2414423},
-	{States:"Maine",Population:1344212,PatientInHospital:594970},
-	{States:"Maryland",Population:6045680,PatientInHospital:2805111},
-	{States:"Massachusetts",Population:6892503,PatientInHospital:3670937},
-	{States:"Michigan",Population:10045029,PatientInHospital:4764339},
-	{States:"Minnesota",Population:5639632,PatientInHospital:2305443},
-	{States:"Mississippi",Population:2976149,PatientInHospital:1442101},
-	{States:"Missouri",Population:6137428,PatientInHospital:3420411},
-	{States:"Montana",Population:1068778,PatientInHospital:400212},
-	{States:"Nebraska",Population:1934408,PatientInHospital:837035},
-	{States:"Nevada",Population:3155070,PatientInHospital:1599975},
-	{States:"New Hampshire",Population:1359711,PatientInHospital:541075},
-	{States:"New Jersey",Population:8882190,PatientInHospital:4367491},
-	{States:"New Mexico",Population:2096829,PatientInHospital:829205},
-	{States:"New York",Population:19453561,PatientInHospital:11946645},
-	{States:"North Carolina",Population:10488084,PatientInHospital:5256731},
-	{States:"North Dakota",Population:762062,PatientInHospital:372056},
-	{States:"Ohio",Population:11689100,PatientInHospital:5747867},
-	{States:"Oklahoma",Population:3956971,PatientInHospital:1917357},
-	{States:"Oregon",Population:4217737,PatientInHospital:1563928},
-	{States:"Pennsylvania",Population:12801989,PatientInHospital:7217096},
-	{States:"Rhode Island",Population:1059361,PatientInHospital:488908},
-	{States:"South Carolina",Population:5148714,PatientInHospital:2490368},
-	{States:"South Dakota",Population:884659,PatientInHospital:420830},
-	{States:"Tennessee",Population:6829174,PatientInHospital:4182394},
-	{States:"Texas",Population:28995881,PatientInHospital:13362245},
-	{States:"Utah",Population:3205958,PatientInHospital:889849},
-	{States:"Vermont",Population:8535519,PatientInHospital:215843},
-	{States:"Virginia",Population:502651,PatientInHospital:3633431},
-	{States:"Washington",Population:7614893,PatientInHospital:2807074},
-	{States:"West Virginia",Population:1792147,PatientInHospital:1055063},
-	{States:"Wisconsin",Population:5822434,PatientInHospital:2330234},
-	{States:"Wyoming",Population:578759,PatientInHospital:122100}
+    {USStates:"Alabama",popHolder:5024279,Population:268332 , StateInitial:"AL"},
+    {USStates:"Alaska",popHolder:731158,Population:2840210 , StateInitial:"AK"},
+	{USStates:"Arizona",popHolder:7378491,Population:2947182 , StateInitial:"AZ"},
+    {USStates:"Arkansas",popHolder:3024692,Population:1460895 , StateInitial:"AR"},
+    {USStates:"California",popHolder:39512223,Population:15165283 , StateInitial:"CA"},
+    {USStates:"Colorado",popHolder:5758736,Population:972907 , StateInitial:"CO"},
+    {USStates:"Connecticut",popHolder:3565287,Population:1815725 , StateInitial:"CT"},
+    {USStates:"Delaware",popHolder:973764,Population:567555 , StateInitial:"DE"},
+    {USStates:"Florida",popHolder:21477737,Population:12807644 , StateInitial:"FL"},
+    {USStates:"Georgia",popHolder:10617423,Population:5158604 , StateInitial:"GA"},
+    {USStates:"Hawaii",popHolder:1415872,Population:540167 , StateInitial:"HI"}, 
+    {USStates:"Idaho",popHolder:1787065,Population:533269 , StateInitial:"ID"},
+    {USStates:"Illinois",popHolder:12671821,Population:5808174 , StateInitial:"IL"},
+    {USStates:"Indiana",popHolder:6732219,Population:3274050 , StateInitial:"IN"},
+    {USStates:"Iowa",popHolder:3155070,Population:1230728 , StateInitial:"IA"},
+    {USStates:"Kansas",popHolder:2913314,Population:1207621 , StateInitial:"KS"},
+    {USStates:"Kentucky",popHolder:4467673,Population:2659947 , StateInitial:"KY"},
+	{USStates:"Louisiana",popHolder:4648794,Population:2414423 , StateInitial:"LA"},
+	{USStates:"Maine",popHolder:1344212,Population:594970 , StateInitial:"ME"},
+	{USStates:"Maryland",popHolder:6045680,Population:2805111 , StateInitial:"MD"},
+	{USStates:"Massachusetts",popHolder:6892503,Population:3670937 , StateInitial:"MA"},
+	{USStates:"Michigan",popHolder:10045029,Population:4764339 , StateInitial:"MI"},
+	{USStates:"Minnesota",popHolder:5639632,Population:2305443 , StateInitial:"MN"},
+	{USStates:"Mississippi",popHolder:2976149,Population:1442101 , StateInitial:"MS"},
+	{USStates:"Missouri",popHolder:6137428,Population:3420411 , StateInitial:"MO"},
+	{USStates:"Montana",popHolder:1068778,Population:400212 , StateInitial:"MT"},
+	{USStates:"Nebraska",popHolder:1934408,Population:837035 , StateInitial:"NE"},
+	{USStates:"Nevada",popHolder:3155070,Population:1599975 , StateInitial:"NV"},
+	{USStates:"New Hampshire",popHolder:1359711,Population:541075 , StateInitial:"NH"},
+	{USStates:"New Jersey",popHolder:8882190,Population:4367491 , StateInitial:"NJ"},
+	{USStates:"New Mexico",popHolder:2096829,Population:829205 , StateInitial:"NM"},
+	{USStates:"New York",popHolder:19453561,Population:11946645 , StateInitial:"NY"},
+	{USStates:"North Carolina",popHolder:10488084,Population:5256731 , StateInitial:"NC"},
+	{USStates:"North Dakota",popHolder:762062,Population:372056 , StateInitial:"ND"},
+	{USStates:"Ohio",popHolder:11689100,Population:5747867 , StateInitial:"OH"},
+	{USStates:"Oklahoma",popHolder:3956971,Population:1917357 , StateInitial:"OK"},
+	{USStates:"Oregon",popHolder:4217737,Population:1563928 , StateInitial:"OR"},
+	{USStates:"Pennsylvania",popHolder:12801989,Population:7217096 , StateInitial:"PA"},
+	{USStates:"Rhode Island",popHolder:1059361,Population:488908  , StateInitial:"RI"},
+	{USStates:"South Carolina",popHolder:5148714,Population:2490368 , StateInitial:"SC"},
+	{USStates:"South Dakota",popHolder:884659,Population:420830 , StateInitial:"SD"},
+	{USStates:"Tennessee",popHolder:6829174,Population:4182394 , StateInitial:"TN"},
+	{USStates:"Texas",popHolder:28995881,Population:13362245 , StateInitial:"TX"},
+	{USStates:"Utah",popHolder:3205958,Population:889849 , StateInitial:"UT"},
+	{USStates:"Vermont",popHolder:8535519,Population:215843 , StateInitial:"VT"},
+	{USStates:"Virginia",popHolder:502651,Population:3633431 , StateInitial:"VA"},
+	{USStates:"Washington",popHolder:7614893,Population:2807074 , StateInitial:"WA"},
+	{USStates:"West Virginia",popHolder:1792147,Population:1055063 , StateInitial:"WV"},
+	{USStates:"Wisconsin",popHolder:5822434,Population:2330234 , StateInitial:"WI"},
+	{USStates:"Wyoming",popHolder:578759,Population:122100 , StateInitial:"WY"}
     ];
 
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 2000 - margin.left - margin.right,
+    width = 2500 - margin.left - margin.right,
     height = 1000 - margin.top - margin.bottom;
 
 var svgUS = d3.select("#barUS")
@@ -118,7 +118,7 @@ var svgUS = d3.select("#barUS")
 
 var x = d3.scaleBand()
   .range([ 0, width ])
-  .domain(data3.map(function(d) { return d.States; }))
+  .domain(data3.map(function(d) { return d.StateInitial; }))
   .padding(0.2);
   svgUS.append("g")
   .attr("transform", "translate(0," + height + ")")
@@ -142,7 +142,7 @@ var y = d3.scaleLinear()
 
   var mouseover = function(d) {
     
-    var StateName = d.States;
+    var StateName = d.USStates;
     var PopulationNum = d.Population;
     tooltip
         .style("opacity", 1)
@@ -165,7 +165,9 @@ var mouseleave = function(d) {
 }
 
 
-  function updateBarDataUS(data,color) {
+  function PartupdateBarDataUS(data,color) {
+
+  y.domain([0, d3.max(data, d => d.popHolder)])
     var u = svgUS.selectAll("rect")
       .data(data)
   
@@ -177,7 +179,31 @@ var mouseleave = function(d) {
       .merge(u)
       .transition()
       .duration(500)
-      .attr("x", function(d) { return x(d.States); })
+     // .attr("x", function(d) { return x(d.USStates); })
+      .attr("y", function(d) { return y(d.Population); })
+     // .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.Population); })
+      
+      .delay(function(d,i){console.log(i) ; return(i*20)})
+      .style("fill", color)
+      .attr("stroke", "grey")
+      .attr("stroke-width", "1.5px")
+  }
+
+  function updateBarDataUS(data,color) {
+
+    var u = svgUS.selectAll("rect")
+      .data(data)
+  
+      u.enter()
+      .append("rect")
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave)
+      .merge(u)
+      .transition()
+      .duration(500)
+      .attr("x", function(d) { return x(d.StateInitial); })
       .attr("y", function(d) { return y(d.Population); })
       .attr("width", x.bandwidth())
       .attr("height", function(d) { return height - y(d.Population); })
